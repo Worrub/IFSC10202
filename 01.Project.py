11ac17a2f3acc763b93ad470e5c03b597715639a
Worrub <kpburrow1@ualr.edu>
2025-09-01 20:14:41 +0000

    update

--- 01.Project.py
+++ 01.Project.py
@@ -1,2 +1,12 @@
 input("Enter Time in Seconds: ")
 #take total seconds and math it out to years, days hours, minutes, and seconds
+Years= 
+Days=
+Hours=
+Minutes=
+Seconds=
+print(Years: )
+print(Days: )
+print(Hours: )
+print(Minutes: )
+print(Seconds: )

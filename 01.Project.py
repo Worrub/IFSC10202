dbdb96d7dd3fda54bc2b2b75d4d72d61048faad8
Worrub <kpburrow1@ualr.edu>
2025-09-01 21:30:15 +0000

    hw update wip

--- 01.Project.py
+++ 01.Project.py
@@ -1,12 +1,13 @@
-input("Enter Time in Seconds: ")
-#take total seconds and math it out to years, days hours, minutes, and seconds
-Years= 
-Days=
-Hours=
-Minutes=
-Seconds=
-print(Years: )
-print(Days: )
-print(Hours: )
-print(Minutes: )
-print(Seconds: )
+x=input("Enter Time in Seconds: ")
+x=int(x)
+Years=int((x//31536000)-=Days)
+Days=int((x//86400)-=Hours)
+Hours=int((x//3600)-=Minutes)
+Minutes=int((x//60)-=Seconds)
+Seconds=int(x//1)
+#take total, divide by seconds in years/hours/days or whatever, then subtract from total and repeat for lesser values 
+print(Years)
+print(Days)
+print(Hours)
+print(Minutes)
+print(Seconds)

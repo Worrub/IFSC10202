fb38292fc2309129639c7cf6cb4a904ac32c10e6
Worrub <kpburrow1@ualr.edu>
2025-09-08 14:35:28 +0000

    update

--- 04.00.08 For Loop Negative Step.py
+++ 04.00.08 For Loop Negative Step.py
@@ -1,2 +1,4 @@
-for i in range(10, 0, -2):
+for i in range(0, 10, -2):
+#for i in range(10, 0, -2):
     print(i)
+    #starting number, ending number, how much taken off per loop
---
+++ 04.01 Product of N Numbers.py
@@ -0,0 +1,7 @@
+#Enter N: 5
+#Enter Number: 4
+#Enter Number: 5
+#Enter Number: 1
+#Enter Number: 3
+#Enter Number: 2
+#120

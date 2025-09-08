d97df2d4303033fc86b0a95086a0a24933b43031
Worrub <kpburrow1@ualr.edu>
2025-09-08 15:44:41 +0000

    done project 3

--- 03.Project.py
+++ 03.Project.py
@@ -1,19 +1,24 @@
-x = input("Enter First Number: ")
+x = float(input("Enter First Number: "))
 operator = input("Enter Operator(+, -, /, or *): ")
-y = input("Enter Second Number: ")
-result = x, operator, y
-
-
-
-
-
-print(x, operator, y = result)
+if operator not in ('+', '-', '/', '*'):
+    print("Invalid Operator")
+y = float(input("Enter Second Number: "))
+if operator == "+":
+    result = x + y
+    print(f"{x} + {y} = {result}")
+elif operator == "-":
+    result = x - y
+    print(f"{x} - {y} = {result}")
+elif operator == "*":
+    result = x * y
+    print(f"{x} * {y} = {result}")
+elif operator == "/":
+    if y != 0:
+        result = x / y
+        print(f"{x} / {y} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
 #Create a simple calculator program and prompts for 2 operands and an operator, then calculates the result.
 #Prompt for the first operand (either integer or floating point)
-#Prompt for a operator, where:
-#+ addition
-#- subtraction
-#* multiplication
-#\ division
-#If an invalid operator is entered, then display "Invalid Operator" (Do not display an invalid calculation)
-#If a valid operator is entered, display the first number, the operand, the second number, an equals sign, and the result of the calculation
+#Prompt for a operator, where:If a valid operator is entered, display the first number, the operand, the second number, an equals sign, and the result of the calculation
+# If an invalid operator is entered, then display "Invalid Operator" (Do not display an invalid calculation)

86fa50e57ff543999fb6b127fae90a9842ba9303
Worrub <kpburrow1@ualr.edu>
2025-09-08 14:57:56 +0000

    update

---
+++ 03.Project.py
@@ -0,0 +1,19 @@
+x = input("Enter First Number: ")
+operator = input("Enter Operator(+, -, /, or *): ")
+y = input("Enter Second Number: ")
+result = x, operator, y
+
+
+
+
+
+print(x, operator, y = result)
+#Create a simple calculator program and prompts for 2 operands and an operator, then calculates the result.
+#Prompt for the first operand (either integer or floating point)
+#Prompt for a operator, where:
+#+ addition
+#- subtraction
+#* multiplication
+#\ division
+#If an invalid operator is entered, then display "Invalid Operator" (Do not display an invalid calculation)
+#If a valid operator is entered, display the first number, the operand, the second number, an equals sign, and the result of the calculation
---
+++ 04.Project.py
@@ -0,0 +1,20 @@
+print()
+
+
+
+
+
+
+
+
+
+##Create a program that displays the following pattern for a given height
+##Hint: Practice for this assignment by completing the 04.07 Ladder exercise
+#Enter maximum height: 4
+#* 
+#* * 
+#* * * 
+#* * * * 
+#* * * 
+#* * 
+#* 

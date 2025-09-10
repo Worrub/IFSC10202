e3771651d76130f47b34e5c9e1deb76b1649d7d7
Worrub <kpburrow1@ualr.edu>
2025-09-10 15:15:49 +0000

    save

--- 05/05.Project.py
+++ 05/05.Project.py
@@ -1,7 +1,9 @@
 
-#Create a program that displays special numbers in a range. A special number is defined to be number. that is the sum of its own digits each raised to the power of the number of digits.
-#153 is 3 digits long and is equal to 13 + 53 + 33
-#8208 is 4 digits long and is equal to 84 + 24 + 04 + 84
+#Create a program that displays special numbers in a range. 
+#A special number is defined to be number that is the sum of its own digits each raised to the power of the number of digits.
+#153 is 3 digits long and is equal to 1^3 + 5^3 + 3^3
+#8208 is 4 digits long and is equal to 8^4 + 2^4 + 0^4 + 8^4
 #First determine the number of digits using a while loop and reminder division by 10. Do not use any string functions or the len function.
 #Recalculate the value using the power from above and see if you get the original value
 #Do not use the len function to determine the number of digits.
+#153 remainder divided gives 3 = number of digits, divided by 10 gives

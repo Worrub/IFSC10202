3cd9ae8a54f061f4a4a79defc1ec81e5f1e656d3
Worrub <kpburrow1@ualr.edu>
2025-09-14 19:02:49 +0000

    p4 save

--- 04/04.Project.py
+++ 04/04.Project.py
@@ -1,13 +1,13 @@
-print()
-
-
-
-
-
-
-
-
-
+n = int(input("Enter Maximum Height", ))
+print("Pattern 1")
+for a1 in range (0,n):
+    for a2 in range (a1):
+        print("* ", end="")
+    print()
+for a1 in range (n,0,-1):
+    for a2 in range (a1):
+        print("* ", end="")
+    print()
 ##Create a program that displays the following pattern for a given height
 ##Hint: Practice for this assignment by completing the 04.07 Ladder exercise
 #Enter maximum height: 4

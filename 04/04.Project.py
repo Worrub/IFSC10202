ba53d2a9d2285efa9040124c5936ab534d93a87b
Worrub <kpburrow1@ualr.edu>
2025-09-14 19:12:03 +0000

    done p4

--- 04/04.Project.py
+++ 04/04.Project.py
@@ -6,11 +6,9 @@ for x in range (0,height):
     print()
 for x in range (height,0,-1):
     for y in range (x):
-        print("*", end=" ")
+         print("*", end=" ")
     print()
 ##Create a program that displays the following pattern for a given height
-##Hint: Practice for this assignment by completing the 04.07 Ladder exercise
-#Enter maximum height: 4
 #* 
 #* * 
 #* * * 

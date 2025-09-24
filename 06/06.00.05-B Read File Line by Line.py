ee6ea1dc2f93e13fd7e862a48686cdbc0eca08a9
Worrub <kpburrow1@ualr.edu>
2025-09-24 14:30:17 +0000

    sdfghjk

--- 06/06.00.05-B Read File Line by Line.py
+++ 06/06.00.05-B Read File Line by Line.py
@@ -1,5 +1,5 @@
 # Open a file for reading
-demotextfile = open("06.00.00 DemoText.txt")
+demotextfile = open("06/06.00.00 DemoText.txt")
 # Read through the file
 for x in demotextfile:
 # Print the contents - Note the imbedded linefeeds
---
+++ 06/06.Project.py
@@ -0,0 +1,35 @@
+# Create a python solution that merges the contents of two files into a new file.
+# Open 06.Project Output File.txt for writing
+# Open and read 06.Project Input File.txt, copying lines from 06.Project Input File.txt to 06.Project Output File.txt line by line. 
+# Keep a count of the number of input records and output records.
+# Inspect each line of the input file for the text: **Insert Merge File Here**
+# At this point, open 06.Project Merge File.txt, copying lines from 06.Project Merge File.txt to 06.Project Output File.txt line by line. 
+# Keep a count of the number of merge records and output records.
+# At the end of 06.Project Merge File.txt, continue copying lines from 06.Project Input File.txt to 06.Project Output File.txt line by line.
+# Keep a count of the number of input records and output records.
+# lose all files
+# Print the number of records in the input file, merge file, and output file.
+inputfilename = "06/06.Project Input File.txt"
+mergefilename = "06/06.Project Merge File.txt"
+outputfilename = "06/06.Project Output File.txt"
+recordcount = 0
+inputfile = open(inputfilename, 'r')
+mergefile = open(mergefilename, 'r')
+outputfile = open(outputfilename, 'w')  
+# Read the first line of the input file
+line = inputfile.readline()
+line = mergefile.readline()
+while line != '':
+# Write to the output file
+# Note that line already contains a linefeed character,
+# so we don't have to add one when we write it.
+     outputfile.write(line)
+     recordcount += 1
+# Read the next line of the input file
+     line = inputfile.readline()
+# End of File on input file
+# Close both files
+inputfile.close()
+mergefile.close()
+outputfile.close()
+print("{} records written".format(recordcount))

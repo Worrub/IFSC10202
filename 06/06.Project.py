68405386550f92b74b2abbc235501c1c10585518
Worrub <kpburrow1@ualr.edu>
2025-10-01 00:51:36 +0000

    fuuuuuuuuuuuuuuuck

--- 06/06.Project.py
+++ 06/06.Project.py
@@ -1,14 +1,5 @@
-# Create a python solution that merges the contents of two files into a new file.
-# Open 06.Project Output File.txt for writing
-# Open and read 06.Project Input File.txt, copying lines from 06.Project Input File.txt to 06.Project Output File.txt line by line. 
-# Keep a count of the number of input records and output records.
-# Inspect each line of the input file for the text: **Insert Merge File Here**
-# At this point, open 06.Project Merge File.txt, copying lines from 06.Project Merge File.txt to 06.Project Output File.txt line by line. 
-# Keep a count of the number of merge records and output records.
-# At the end of 06.Project Merge File.txt, continue copying lines from 06.Project Input File.txt to 06.Project Output File.txt line by line.
-# Keep a count of the number of input records and output records.
-# lose all files
-# Print the number of records in the input file, merge file, and output file.
+#Fix code only overwriting line 1 of input and only with line 1 of merge into output
+#Whelp there goes all my GD progress
 inputfilename = "06/06.Project Input File.txt"
 mergefilename = "06/06.Project Merge File.txt"
 outputfilename = "06/06.Project Output File.txt"
